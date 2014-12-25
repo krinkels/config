34f552421e1fe45f43f4b970077650367379360c
Wayne Lu <wayne.lu.8@gmail.com>
2014-12-24 23:56:16 -0500

    Adding messages to setup script

--- setup.sh
+++ setup.sh
@@ -2,19 +2,24 @@
 
 # copy in new config files
 if [ -f ~/.bash_profile ]; then
+    echo Copying .bash_profile to .bash_profile.old...
     mv ~/.bash_profile ~/.bash_profile.old
 fi
 
 if [ -f ~/.vimrc ]; then
+    echo Copying .vimrc to .vimrc.old...
     mv ~/.vimrc ~/.vimrc.old
 fi
 
+echo Creating symlinks...
 ln -s ~/config/bash_profile ~/.bash_profile
 ln -s ~/config/vimrc ~/.vimrc
 
 # get vcprompt
 if [ ! -f ~/bin/vcprompt ]; then
+    echo Downloading vcprompt...
     mkdir ~/bin
     curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt > ~/bin/vcprompt
     chmod 755 ~/bin/vcprompt
 fi
+echo Done!
